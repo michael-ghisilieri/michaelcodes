097ebede4e44943f2acb81bc03866dd67312298a
Michael Ghisilieri <ghisilie@unlv.nevada.edu>
2018-06-08 18:20:28 -0700

    header and image work together finally

--- src/App.js
+++ src/App.js
@@ -8,49 +8,34 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <header>
-        <div className="container">
-          <h3 className="name">michael ghisilieri</h3>
-          <navi>
-            <ul>
-              <li>
-                <Link to="/about-me" className="custom-links">about me</Link>
-              </li>
-              <li>
-                <Link to="/games" className="custom-links">games</Link>
-              </li>
-              <li>
-                <Link to="/projects" className="custom-links">projects</Link>
-              </li>
-            </ul>
-          </navi>
-        {/*
-        <Navbar className="navbar-custom" fluid>
-          <Navbar.Header>
-            <Navbar.Brand className="navbar-custom"> 
-              <Link to="/home">michael ghisilieri</Link>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <ButtonToolbar>
-              <Link to="/about-me">
-                <Button className="button-margin btn btn-light btn-custom">About Me</Button>
-              </Link>
-              <Link to="/games">
-                <Button className="button-margin btn btn-light btn-custom">Games</Button>
-              </Link>
-              <Link to="/projects">
-                <Button className="button-margin btn btn-light btn-custom">Projects</Button>
-              </Link>
-              <Button className="btn btn-light btn-custom">
-                <img className="button-margin" src={ require('./images/menu.png') } alt="menu"/> Menu
-              </Button>
-            </ButtonToolbar>
-          </Nav>
-        </Navbar>
-        {/*<hr />*/}
-        </div>
-      </header>
+      <body>
+        <header>
+          <div className="container">
+
+            <h3 className="name">michael ghisilieri</h3>
+
+            <navi>
+              <ul>
+                <li>
+                  <Link to="/about-me" className="custom-links">about me</Link>
+                </li>
+                <li>
+                  <Link to="/games" className="custom-links">games</Link>
+                </li>
+                <li>
+                  <Link to="/projects" className="custom-links">projects</Link>
+                </li>
+              </ul>
+            </navi>
+          </div>
+        </header>
+
+        <img className="header-img" src={ require('./images/orangejustice_crop.png') } alt="orange justice"/>
+
+      <h2><p>heeeeeeelp</p></h2>
+
+      </body>
+
     );
   }
 }
