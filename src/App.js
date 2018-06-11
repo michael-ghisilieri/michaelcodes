18a3e2726be728fde6a58a3098ac99d7fe0bd540
Martin Jaime <martin.jaime-viveros@originate.com>
2018-06-10 23:38:53 -0700

    fix after merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-//import { Navbar, Nav, Button, ButtonToolbar } from 'react-bootstrap';
-import Header from './header';
-import Panel from './components/Panels/Panel';
-import codephoto from './images/code.jpg';
-import indigo from './images/indigo-logo.png';
-import './App.css';
+import React, { Component } from 'react'
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import { Navbar, Nav, Button, ButtonToolbar } from 'react-bootstrap';
+import Header from './header'
+import Panel from './components/Panels/Panel'
+import codephoto from './images/code.jpg'
+import indigo from './images/indigo-logo.png'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -13,21 +13,24 @@ class App extends Component {
       <body>
         <Header />
 
-        <img className="header-img" src={codephoto} alt="orange justice"/>
+        <img className="header-img" src={codephoto} alt="orange justice" />
 
         <about>
           <h2>About Me</h2>
-          <div className="line-bar"></div>
+          <div className="line-bar" />
 
           <div>
-            <p>Computer Engineer graduate who has found a new love for web development and design. Hop along for the ride.</p>
+            <p>
+              Computer Engineer graduate who has found a new love for web
+              development and design. Hop along for the ride.
+            </p>
           </div>
         </about>
 
-        <Panel image={indigo} imgPos='left' txtPos='right'/>
-        <Panel image={indigo} imgPos='right' txtPos='left'/>
-        <Panel image={indigo} imgPos='left' txtPos='right'/>
-        <Panel image={indigo} imgPos='right' txtPos='left'/>
+        <Panel image={indigo} imgPos="left" txtPos="right" />
+        <Panel image={indigo} imgPos="right" txtPos="left" />
+        <Panel image={indigo} imgPos="left" txtPos="right" />
+        <Panel image={indigo} imgPos="right" txtPos="left" />
       </body>
     )
   }
