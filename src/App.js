10e8a4a1aed973866521e39dc21753b698dbb304
Michael Ghisilieri <ghisilie@unlv.nevada.edu>
2018-07-02 14:45:31 -0700

    homepage render and css moved from App component to Home component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,15 @@
-import React, { Component } from 'react'
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import { Navbar, Nav, Button, ButtonToolbar } from 'react-bootstrap';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import history from './history'
+import routes from './routes'
 import Header from './header'
 import Footer from './footer'
-import Panel from './components/Panels/Panel'
-import Card from './components/Cards/Card'
-import Error from './components/Error/Error'
-import codephoto from './images/code.jpg'
-//import indigo from './images/indigo-logo.png'
-import webpage from './images/webpage.png'
-import tools from './images/tools.png'
-import recordplayer from './images/recordplayer.png'
-import computer from './images/computer-desk.jpg'
-//import glass from './images/magnify-blur.jpg'
-import guitar from './images/guitar-catcher.jpg'
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-
-        <img className="header-img" src={codephoto} alt="orange justice" />
-
-        <div className="about">
-          <h2>About Me</h2>
-          <div className="line-bar" />
-
-          <div>
-            <p>
-              Computer Engineer graduate who has found a new love for web
-              development and design. Hop along for the ride.
-            </p>
-          </div>
-        </div>
-
-        <div className="card-box">
-          <Card image={webpage} title="Web Development" content="Found a new love for web development and making the internet look a little more exciting." />
-          <Card image={tools} title="Building Things" content="Building more than just websites with basically anything that sparks my interest at the time." />
-          <Card image={recordplayer} title="Music Is Good Too" content="I think 14 years of playing guitar has earned me the right to say I'm pretty into music, no?" />
-        </div>
-
-        <div className="panel-box">
-          <Panel
-            image={computer} title="Michael Ghisilieri" imgPos="left" txtPos="right"
-            text="Basically living on my computer, I spend my time focusing on becoming a web designer and developer nowadays. There's just something intriguing about learning with tools like ReactJS that combines programming with the creativity to have complete control of an entire website. To find out more about the work that I have done in the past and am currently working towards, check out <a href='/professional'>professional</a>."
-          />
-          <Panel 
-            image={guitar} title='' imgPos="right" txtPos="left"
-            text="When I'm not building, designing, and learning stuff on my computer, I like to expand my music library. Being a video game collector also keeps me occupied every now and then. To give everyone a more personal look into my life, I'll update things such as what I'm currently listening to for the week and maybe game pickups over on <a href='/personal'>personal</a>. Who knows, maybe some guitar covers will start popping up along the way too."  
-          />
-        </div>
-
-        <Footer />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div />
+  )
 }
 
 export default App
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react'
+import Panel from '../Panels/Panel'
+import Card from '../Cards/Card'
+import codephoto from '../../images/code.jpg'
+import webpage from '../../images/webpage.png'
+import tools from '../../images/tools.png'
+import recordplayer from '../../images/recordplayer.png'
+import computer from '../../images/computer-desk.jpg'
+import guitar from '../../images/guitar-catcher.jpg'
 import './Home.css'
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div className="banner">
-          <img
-            src={require('../../images/header-background.jpg')}
-            alt="header background"
-          />
-          <div className="segoe-font top-text">Hi! I'm Michael Ghisilieri</div>
-          <div className="segoe-font bottom-text">
-            <i>Software Engineer &amp; Web Developer</i>
+
+        <img className="header-img" src={codephoto} alt="orange justice" />
+
+        <div className="about">
+          <h2>About Me</h2>
+          <div className="line-bar" />
+
+          <div>
+            <p>
+              Computer Engineer graduate who has found a new love for web
+              development and design. Hop along for the ride.
+            </p>
           </div>
         </div>
+
+        <div className="card-box">
+          <Card image={webpage} title="Web Development" content="Found a new love for web development and making the internet look a little more exciting." />
+          <Card image={tools} title="Building Things" content="Building more than just websites with basically anything that sparks my interest at the time." />
+          <Card image={recordplayer} title="Music Is Good Too" content="I think 14 years of playing guitar has earned me the right to say I'm pretty into music, no?" />
+        </div>
+
+        <div className="panel-box">
+          <Panel
+            image={computer} title="Michael Ghisilieri" imgPos="left" txtPos="right"
+            text="Basically living on my computer, I spend my time focusing on becoming a web designer and developer nowadays. There's just something intriguing about learning with tools like ReactJS that combines programming with the creativity to have complete control of an entire website. To find out more about the work that I have done in the past and am currently working towards, check out <a href='/professional'>professional</a>."
+          />
+          <Panel 
+            image={guitar} title='' imgPos="right" txtPos="left"
+            text="When I'm not building, designing, and learning stuff on my computer, I like to expand my music library. Being a video game collector also keeps me occupied every now and then. To give everyone a more personal look into my life, I'll update things such as what I'm currently listening to for the week and maybe game pickups over on <a href='/personal'>personal</a>. Who knows, maybe some guitar covers will start popping up along the way too."  
+          />
+        </div>
+
       </div>
     )
   }
