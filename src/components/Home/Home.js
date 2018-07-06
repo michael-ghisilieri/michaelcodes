643829a3e39a24e81ebd2cf7d0e85325aa556283
Michael Ghisilieri <ghisilie@unlv.nevada.edu>
2018-07-05 18:03:39 -0700

    moved text blocks to content.json file to clean up Home.js, edited actual content of text blocks, had to make minor tweaks to Panel.css due to change in text length

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,6 +8,7 @@ import recordplayer from '../../images/recordplayer.png'
 import computer from '../../images/computer-desk.jpg'
 import guitar from '../../images/guitar-catcher.jpg'
 import './Home.css'
+import textData from '../../content.json'
 
 class Home extends Component {
   render() {
@@ -23,25 +24,25 @@ class Home extends Component {
           <div>
             <p>
               Computer Engineer graduate who has found a new love for web
-              development and design. Hop along for the ride.
+              development. This website starts the journey. Hop along for the ride.
             </p>
           </div>
         </div>
 
         <div className="card-box">
-          <Card image={webpage} title="Web Development" content="Found a new love for web development and making the internet look a little more exciting." />
-          <Card image={tools} title="Building Things" content="Building more than just websites with basically anything that sparks my interest at the time." />
-          <Card image={recordplayer} title="Music Is Good Too" content="I think 14 years of playing guitar has earned me the right to say I'm pretty into music, no?" />
+          <Card image={webpage} title="Web Development" content={textData.homeCard1} />
+          <Card image={tools} title="Building Things" content={textData.homeCard2} />
+          <Card image={recordplayer} title="Music Is Good Too" content={textData.homeCard3} />
         </div>
 
         <div className="panel-box">
           <Panel
             image={computer} title="Michael Ghisilieri" imgPos="left" txtPos="right"
-            text="Basically living on my computer, I spend my time focusing on becoming a web designer and developer nowadays. There's just something intriguing about learning with tools like ReactJS that combines programming with the creativity to have complete control of an entire website. To find out more about the work that I have done in the past and am currently working towards, check out <a href='/professional'>professional</a>."
+            text={textData.homePanel1}
           />
           <Panel 
             image={guitar} title='' imgPos="right" txtPos="left"
-            text="When I'm not building, designing, and learning stuff on my computer, I like to expand my music library. Being a video game collector also keeps me occupied every now and then. To give everyone a more personal look into my life, I'll update things such as what I'm currently listening to for the week and maybe game pickups over on <a href='/personal'>personal</a>. Who knows, maybe some guitar covers will start popping up along the way too."  
+            text={textData.homePanel2}  
           />
         </div>
 
