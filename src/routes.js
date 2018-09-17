a222f43a93e50ea8ca28ca18ed23981b7295180e
Michael Ghisilieri <ghisilie@unlv.nevada.edu>
2018-09-17 03:55:33 -0700

    About and Professional routes added. background pattern added to whole website

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import About from './components/About/About'
 import Header from './header'
 import Footer from './footer'
 import Error404 from './components/Error/Error'
+import Professional from './components/Professional/Professional'
 
 const createRoutes = () => {
   return (
@@ -16,7 +17,8 @@ const createRoutes = () => {
         <div className="body-container">
           <Switch>
             <Route exact path="/" component={Home} />
-            {/*<Route path="/about" component={About} />*/}
+            <Route path="/about" component={About} />
+            <Route path="/professional" component={Professional} />
             <Route path="*" component={Error404} />
           </Switch>
         </div>
