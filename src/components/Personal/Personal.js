9360cd130f00cc5e49e737ce73eb216825aa9826
Michael Ghisilieri <ghisilie@unlv.nevada.edu>
2018-09-23 21:53:47 -0700

    personal page template

---
+++ src/components/Personal/Personal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Personal.css'
+
+class Personal extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="segoe-font">Games</h2>
+      </div>
+    )
+  }
+}
+
+export default Personal
