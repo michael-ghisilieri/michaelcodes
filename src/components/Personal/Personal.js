ed5df3078260029c048cf91aa1990c95b59511a6
Michael Ghisilieri <ghisilie@unlv.nevada.edu>
2018-12-18 21:56:19 -0800

    four tiles added with unique fonts, one image not the correct size

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 import './Personal.css'
+import Tile from '../Tile/Tile'
+import horror from '../../images/horrorCandles.jpg'
+import sports from '../../images/football.jpg'
+import music from '../../images/musicDJ.jpg'
+import gaming from '../../images/ps4Controller.jpg'
 
 class Personal extends Component {
   render() {
     return (
-      <div>
-        <h1>Personal Page</h1>
+      <div className="personal-page">
+        <Tile image={music} title="music" alt="horror-tile" />
+        <Tile image={sports} title="sports" alt="horror-tile" />
+        <Tile image={horror} title="horror" alt="horror-tile" />
+        <Tile image={gaming} title="gaming" alt="horror-tile" />
+        <div className="clr" />
+
+        <h1 id="music-section" className="monitor-font">
+          MUSIC
+        </h1>
+        <h1 id="sports-section" className="sports-font">
+          SPORTS
+        </h1>
+        <h1 id="horror-section" className="october-font">
+          HORROR
+        </h1>
+        <h1 id="gaming-section" className="arcade-font">
+          GAMING
+        </h1>
       </div>
     )
   }
